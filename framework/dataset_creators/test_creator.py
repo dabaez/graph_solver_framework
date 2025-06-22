e3ed635584680838ccce9751f3f719264235e073
dabaez <mastil.daniel@gmail.com>
2025-06-22 18:26:59 -0400

    test dataset creator

---
+++ framework/dataset_creators/test_creator.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+import networkx as nx
+
+from framework.core.dataset_creator import RequiredParameter
+from framework.core.graph import Dataset, DatasetFromNetworkX
+
+
+class TestDatasetCreator:
+    @property
+    def required_parameters(self) -> list[RequiredParameter]:
+        return []
+
+    def validate_parameters(self, parameters: dict[str, Any]) -> bool:
+        if parameters:
+            return False
+        return True
+
+    def create_dataset(self, parameters: dict[str, Any]) -> Dataset:
+        G = nx.Graph()
+        G.add_nodes_from(["A", "B", "C", "D", "E"])
+        G.add_edges_from(
+            [
+                ("A", "B"),
+                ("A", "E"),
+                ("E", "B"),
+                ("E", "D"),
+                ("D", "B"),
+                ("B", "C"),
+                ("C", "D"),
+            ]
+        )
+        return DatasetFromNetworkX([G])
