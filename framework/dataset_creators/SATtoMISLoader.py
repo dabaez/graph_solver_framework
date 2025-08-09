2922431297e168e6f4964cc505b90ab81532b4a8
dabaez <mastil.daniel@gmail.com>
2025-08-09 17:55:05 -0400

    sat to mis graph loader

---
+++ framework/dataset_creators/SATtoMISLoader.py
@@ -0,0 +1,76 @@
+import os
+
+import networkx as nx
+import numpy as np
+from pysat.formula import CNF
+
+from framework.core.dataset_creator import RequiredParameter
+from framework.core.graph import Dataset, Feature, FrameworkGraph
+from framework.core.registries import register_dataset_creator
+
+
+@register_dataset_creator("SATtoMISLoader")
+class SATtoMISLoader:
+    def description(self) -> str:
+        return "Loads a dataset of SAT instances and converts them to Maximum Independent Set (MIS) instances."
+
+    def required_parameters(self) -> list[RequiredParameter]:
+        return [
+            RequiredParameter(
+                name="folder path",
+                description="Path to the folder containing the SAT instance files. The instances should be in DIMACS cnf format.",
+            ),
+        ]
+
+    def validate_parameters(self, parameters: dict[str, str]) -> bool:
+        if "folder path" not in parameters:
+            return False
+        folder_path = parameters["folder path"]
+        return os.path.isdir(folder_path)
+
+    def create_dataset(self, parameters: dict[str, str]) -> Dataset:
+        folder_path = parameters["folder path"]
+        dataset: Dataset = Dataset([])
+
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".cnf"):
+                file_path = os.path.join(folder_path, filename)
+                graph = self.convert_sat_to_mis(file_path)
+                framework_graph = FrameworkGraph(graph)
+                framework_graph.add_feature(
+                    Feature(name="source_file", value=filename), overwrite=False
+                )
+                dataset.append(framework_graph)
+
+        return dataset
+
+    # taken from mis benchmark framework https://github.com/MaxiBoether/mis-benchmark-framework/blob/master/data_generation/sat.py
+    def convert_sat_to_mis(self, file_path: str) -> nx.Graph:
+        """Convert a SAT instance in DIMACS cnf format to a Maximum Independent Set (MIS) instance."""
+        cnf = CNF(from_file=file_path)
+        nv = cnf.nv
+        clauses = list(filter(lambda x: x, cnf.clauses))
+        ind = {
+            k: [] for k in np.concatenate([np.arange(1, nv + 1), -np.arange(1, nv + 1)])
+        }
+        edges = []
+        for i, clause in enumerate(clauses):
+            a = clause[0]
+            b = clause[1]
+            c = clause[2]
+            aa = 3 * i
+            bb = 3 * i + 1
+            cc = 3 * i + 2
+            ind[a].append(aa)
+            ind[b].append(bb)
+            ind[c].append(cc)
+            edges.append((aa, bb))
+            edges.append((aa, cc))
+            edges.append((bb, cc))
+
+        for i in range(1, nv + 1):
+            for u in ind[i]:
+                for v in ind[-i]:
+                    edges.append((u, v))
+
+        return nx.from_edgelist(edges)
--- framework/dataset_creators/__init__.py
+++ framework/dataset_creators/__init__.py
@@ -1,2 +1,3 @@
 from .BarabasiAlbertGenerator import BarabasiAlbertGenerator
+from .SATtoMISLoader import SATtoMISLoader
 from .TestCreator import TestDatasetCreator
